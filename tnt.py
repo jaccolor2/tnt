eee431137919b606668767601190b3ad182356ce
jaccolor2 <98039916+jaccolor2@users.noreply.github.com>
2024-06-17 16:55:51 +0200

    Update tnt.py

--- tnt.py
+++ tnt.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, send_file
 import base64
 import tempfile
+import os
 
 app = Flask(__name__)
 
@@ -28,4 +29,5 @@ def generate_pdf():
         return f"Error generating PDF: {str(e)}", 500
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=True)
